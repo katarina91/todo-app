d34a325e690e2b51d8de334e39891ea564421213
Katarina Jovanovic <kacaj91@gmail.com>
2016-02-20 16:37:01 +0100

    function from unerscore module is added

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -7,45 +9,59 @@ var todoNextId = 1;
 
 app.use(bodyParser.json());
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
 
 //GET /todos
-app.get('/todos', function(req, res){
+app.get('/todos', function (req, res) {
 	res.json(todos);//convert to json
 });
 
 //GET /todos/:id
-app.get('/todos/:id', function(req, res){
+app.get('/todos/:id', function (req, res) {
 	var todoId = req.params.id;
+	//var matchedTodo = _.findWhere(todos, {id: todoId});
 	var matchedTodo;
+
+	todos.forEach(function (todo) {
+		if (todo.id == todoId) {
+			matchedTodo = todo;
+		}
+	});
+	
+	if (matchedTodo) {
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send();
+	}
+	/*var matchedTodo;
 	
 	todos.forEach(function(todo){
 		if(todo.id == todoId){
 			matchedTodo = todo;
 		}
 	});
-	
-	if(matchedTodo){
-		res.json(matchedTodo);
-	}
-	else{
-		res.status(404).send();	
-	}
+	*/
 });
 
 // POST /todos
-app.post('/todos', function(req, res){
-	var body = req.body;
+app.post('/todos', function (req, res) {
+	//var body = req.body;
+	var body = _.pick(req.body, "description", "completed"); //which parameters will be posted
 	
-	body.id = todoNextId;
-	todoNextId++;
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+	
+	body.description = body.description.trim();
+	body.id = todoNextId++;
+	
 	todos.push(body);
-	
+
 	res.json(body);
 });
 
-app.listen(PORT, function(){
+app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT + '!');
 });
