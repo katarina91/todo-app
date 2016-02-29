b11290cb593269d8de12195ee10aae10223717bd
Katarina Jovanovic <kacaj91@gmail.com>
2016-02-29 21:24:31 +0100

    Convert PUT /todos/:id to sequelize

--- server.js
+++ server.js
@@ -18,22 +18,22 @@ app.get('/', function (req, res) {
 app.get('/todos', function (req, res) {
 	var query = req.query;
 	var where = {};
-	
+
 	if (query.hasOwnProperty('completed') && query.completed === 'true') {
 		where.completed = true;
-	}else if (query.hasOwnProperty('completed') && query.completed === 'false') {
+	} else if (query.hasOwnProperty('completed') && query.completed === 'false') {
 		where.completed = false;
 	}
-	
-	if (query.hasOwnProperty('q') && query.q.length > 0){
+
+	if (query.hasOwnProperty('q') && query.q.length > 0) {
 		where.description = {
 			$like: '%' + query.q + '%'
 		};
 	}
-	
-	db.todo.findAll({where:where}).then(function(todos){
+
+	db.todo.findAll({ where: where }).then(function (todos) {
 		res.json(todos);
-	}, function(e){
+	}, function (e) {
 		res.status(500).send();
 	});
 });
@@ -41,15 +41,15 @@ app.get('/todos', function (req, res) {
 //GET /todos/:id
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	
-	db.todo.findById(todoId).then(function(todo){
-		if(todo){
+
+	db.todo.findById(todoId).then(function (todo) {
+		if (todo) {
 			res.json(todo.toJSON());
 		}
-		else{
+		else {
 			res.status(404).send();
 		}
-	}, function(e){
+	}, function (e) {
 		res.status(500).send();
 	});
 });
@@ -58,9 +58,9 @@ app.get('/todos/:id', function (req, res) {
 app.post('/todos', function (req, res) {
 	var body = _.pick(req.body, "description", "completed"); //which parameters will be posted
 	
-	db.todo.create(body).then(function(todo){
+	db.todo.create(body).then(function (todo) {
 		res.json(todo.toJSON());
-	}, function(e){
+	}, function (e) {
 		res.status(400).json();
 	});
 });
@@ -68,50 +68,54 @@ app.post('/todos', function (req, res) {
 //DELETE /todos/:id
 app.delete('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	
+
 	db.todo.destroy({
 		where: {
 			id: todoId
 		}
-	}).then(function(rowsDeleted){
-		if(rowsDeleted === 0){
+	}).then(function (rowsDeleted) {
+		if (rowsDeleted === 0) {
 			res.status(404).json({ 'error': 'no todo with that id' });
 		}
-		else{
+		else {
 			res.status(204).send();
 		}
-	}, function(){
+	}, function () {
 		res.status(500).send();
 	});
-	
+
 });
 
 //PUT /todos/:id
 app.put('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, { id: todoId });
 	var body = _.pick(req.body, "description", "completed"); //which parameters will be posted
-	var validAttributes = {};
+	var attributes = {};
 
-	if (!matchedTodo) {
-		return res.status(404).send();
+
+	if (body.hasOwnProperty('completed')) {
+		attributes.completed = body.completed;
 	}
 
-	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-		validAttributes.completed = body.completed;
-	} else if (body.hasOwnProperty('completed')) {
-		return res.status(400).send();
+	if (body.hasOwnProperty('description')) {
+		attributes.description = body.description;
 	}
 
-	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-		validAttributes.description = body.description;
-	} else if (body.hasOwnProperty('description')) {
-		return res.status(400).send();
-	}
+	db.todo.findById(todoId).then(function (todo) {
+		if (todo) {
+			todo.update(attributes).then(function (todo) {
+				res.json(todo.toJSON())
+			}, function (e) {
+				res.status(400).json(e);
+			});
+		}
+		else {
+			res.status(404).send();
+		}
+	}, function () {
+		res.status(500).send();
+	});
 
-	_.extend(matchedTodo, validAttributes);//copy object into object
-	
-	res.json(matchedTodo);
 });
 
 db.sequelize.sync().then(function () {
