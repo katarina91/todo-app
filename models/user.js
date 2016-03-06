7263334c4d1f3d75e6846e04df9c265cd254456e
Katarina Jovanovic <kacaj91@gmail.com>
2016-03-06 13:00:17 +0100

    generation token fot POST /users/login

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 //var bcrypt = require('bcrypt');
 var _ = require('underscore');
+var cryptojs = require('crypto-js');
+var jwt = require('jsonwebtoken');
 
 module.exports = function (sequelize, DataTypes) {
 	var user = sequelize.define('user', {
@@ -54,6 +56,23 @@ module.exports = function (sequelize, DataTypes) {
 				toPublicJSON: function () {
 					var json = this.toJSON();
 					return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
+				},
+				generateToken: function(type){
+					if(!_.isString(type)){
+						return undefined;
+					}
+					
+					try{
+						var stringData = JSON.stringify({id: this.get('id'), type:type});
+						var encryptedData = cryptojs.AES.encrypt(stringData, '123abcd').toString();
+						var token = jwt.sign({
+							token: encryptedData
+						}, '123abcd');
+						
+						return token;
+					} catch (e){
+						return undefined;
+					}
 				}
 			}
 		});
